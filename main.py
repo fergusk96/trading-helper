5b50987adcfaa09a5eedaf98e5ae9bd4ae543d09
Fergus Kavanagh <fergus.kavanagh@glovoapp.com>
2024-04-06 00:40:17 +0200

    expose method correctly

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ class TradeHelperApp:
     def __init__(self):
         self.trade_retrieval_service = TradeRetrievalService()
 
-    def get_trade(self, request):
+    def get_trade_by_id(self, request):
         return self.trade_retrieval_service.get_trade_by_id(request["trade_id"])
 
 
 helper_app = TradeHelperApp()
-get_trade_by_id = helper_app.get_trade
+
+
+def get_trade_by_id(request):
+    return helper_app.get_trade_by_id(request)
